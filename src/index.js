43cdda8877855ecde76afa2b1776493ccb70e2aa
Or <orshadad@gmail.com>
2019-11-17 21:03:02 +0200

    push all files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+function App() {
+  const [userData, setUserData] = useState({})
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  async function getUserData() {
+    const key = 'b0b8cd08bd4c1824cd4df64ab44f6975fc71e6ba25914603fe048c61';
+    const res = await axios.get(`https://api.ipdata.co?api-key=${key}`);
+    setUserData(
+      { 
+        city: res.data.city,
+        country_name: res.data.country_name,
+        latitude: res.data.latitude,
+        longitude: res.data.longitude,
+      });
+  }
+
+  function getMap() {
+    const key = 'AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk';
+    return (
+      <iframe
+        title="location"
+        className="map"
+        src={`https://www.google.com/maps/embed/v1/place?key=${key}&q=${userData.latitude},${userData.longitude}`}
+        allowFullScreen>
+      </iframe>
+    )
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello there in {userData.city}, {userData.country_name}</h1>
+      <h2>This is where you are on the map:</h2>
+      {getMap()}
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
